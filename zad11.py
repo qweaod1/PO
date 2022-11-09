5dbf9a5c270ad202536724d0bd6553b01cee3c97
Kamil_Andruszkiewicz <76594488+qweaod1@users.noreply.github.com>
2022-11-09 12:05:47 +0100

    Update zad11.py

--- zad11.py
+++ zad11.py
@@ -1,13 +1,13 @@
 class Student:
-    name = 'Piter'
+    name = 'Kamil'
     school = 'UWM WMiI'
 
 student_1 = Student()
 student_2 = Student()
-student_1.name = 'Mango'
-student_1.school = 'School at Bahamas'
-student_2.name = 'Potato'
-student_2.school = 'School at Pole'
+student_1.name = 'Banana'
+student_1.school = 'Uniwersytet warminsko mazurski'
+student_2.name = 'Orange'
+student_2.school = 'School'
 students = [student_1, student_2]
 for student in students:
     for attr in student.__dict__:
