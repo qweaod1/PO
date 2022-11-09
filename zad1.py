c990ee9657a90310a2abad85d8ee87d5ebb06c3b
Kamil_Andruszkiewicz <76594488+qweaod1@users.noreply.github.com>
2022-11-09 11:50:41 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -3,16 +3,6 @@ print("Zad1\n")
 for name in array.__dict__:
     print(name)
 
-print("\nZad2\n")
-class Zad2:
-    def __init__(self, imie):
-        self.imie = imie
-    def wyswietl(self, imie):
-        return imie
-
-for name in Zad2.__dict__:
-    print(name)
-
 print("\nZad3\n")
 imie = Zad2('Piotr')
 print(imie.__dict__)
