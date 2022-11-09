4ba5437f3e8d7b43e0b299457ba21095113b2d2b
Kamil_Andruszkiewicz <76594488+qweaod1@users.noreply.github.com>
2022-11-09 12:01:53 +0100

    Update zad10.py

--- zad10.py
+++ zad10.py
@@ -1,11 +1,11 @@
 class Student:
-    student_id = '162439'
-    student_name = 'Piter'
+    student_id = '166228'
+    student_name = 'Kamil'
 
 print(f"Student Name: {getattr(Student, 'student_name')}")
 print(f"Student id: {getattr(Student, 'student_id')}\n")
 
-Student.student_class = '2IO'
+Student.student_class = '1IO'
 for attr, values in Student.__dict__.items():
     if not attr.startswith('__'):
         print(f"{attr} -> {values}")
